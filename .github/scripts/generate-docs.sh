564198f45379072b7d6f5a7c0792bfc02b42d469
parradafe-glbnt <Daniel.Parra@greatminds.org>
2024-09-11 10:10:33 -0500

    integrated shell to generate multiple docs at the same time

---
+++ .github/scripts/generate-docs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ../../
+for PACKAGE in $(ls)
+do
+    echo "=>> $PACKAGE"
+    cd $PACKAGE
+    if [ -e './node_modules' ]
+    then
+        npm run docs
+    fi
+    cd ..
+    echo "docs generated for : $PACKAGE"
+done
